6c074d6161bbe6fed4c0a3bb1e930dc4046f9dcf
Ted S <t3ddftw@users.noreply.github.com>
2015-12-11 11:26:34 -0800

    Update email.py

--- m3scout/lib/email.py
+++ m3scout/lib/email.py
@@ -3,13 +3,13 @@ import smtplib
 
 class Email(object):
     
-    EMAIL_ADDY = 'tass2001@gmail.com'
+    EMAIL_ADDY = 'someone@gmail.com'
     
     def __init__(self, ):
         self.SMTP = smtplib.SMTP('smtp.gmail.com', 587)
         self.SMTP.starttls()
         # App specific, GLHF
-        self.SMTP.login(self.EMAIL_ADDY, 'hfyrmomniflqhtnq')
+        self.SMTP.login(self.EMAIL_ADDY, 'somepass')
     
     
     def send_email(self, to, body, ):
